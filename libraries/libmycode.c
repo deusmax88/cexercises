537648c4945eb5aa2a311fd87b1eeafc36e8de03
mark <deusmax88@gmail.com>
2020-07-20 00:30:37 +0500

    Added libraries section

---
+++ libraries/libmycode.c
@@ -0,0 +1,14 @@
+#include "libmycode.h"
+#include <string.h>
+
+char* reverse(char* string) {
+    int len = strlen(string);
+
+    for(int i = 0; i < len/2; i++) {
+        char tmp = string[i];
+        string[i] = string[len - 1 - i];
+        string[len - 1 -i] = tmp;
+    }
+
+    return string;
+}
---
+++ libraries/libmycode.h
@@ -0,0 +1,6 @@
+#ifndef __LIB_MY_CODE_
+#define __LIB_MY_CODE_
+
+char* reverse(char* string);
+
+#endif
