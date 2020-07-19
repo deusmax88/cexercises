eca3b9ca7dcefadb169306794e1b77036ec30f5d
mark <deusmax88@gmail.com>
2020-07-19 22:57:15 +0500

    Core dump example

---
+++ core_dump/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <time.h>
+
+int main () {
+    int rnum = 0;
+    int* goodptr = malloc(sizeof(int));
+    int* badptr = NULL;
+
+    srand(time(NULL));
+    while(true) {
+        rnum = rand() % 255;
+        if (rnum == 13) {
+            *badptr = rnum;
+        }
+        else {
+            *goodptr = rnum;
+        }
+        printf("random: %d\n", rnum);
+        usleep(100);
+    }
+}
