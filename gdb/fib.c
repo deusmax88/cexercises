52bb2a558dacb7bad095dbff8ce4e1d94af5c3ef
mark <deusmax88@gmail.com>
2020-07-24 12:29:52 +0500

    Added fibonachi numbers

---
+++ gdb/fib.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fib (int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+
+    return fib(n - 1) + fib(n - 2);
+}
+
+int main(int argc, char** argv) {
+    printf("%d", fib(atoi(argv[1])));
+}
